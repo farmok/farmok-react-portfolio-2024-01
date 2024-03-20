6aeb307c7e51ff28f78ff47f6c96a52c6a1d2f97
Farid Mokraoui <farmok@gmail.com>
2024-03-20 11:47:07 -0400

    Finished Leadership Page and Started Home Page

--- react-portfolio-contentful/src/pages/home.jsx
+++ react-portfolio-contentful/src/pages/home.jsx
@@ -3,47 +3,21 @@ import useContentful from '../hooks/use_contentful';
 import styles from '../assets/styles/main.module.scss';
 import Header from '../components/layout/header';
 import Footer from '../components/layout/footer';
-import Hero from '../components/layout/project_hero';
-import Topic from '../components/common/topic';
+import Hero from '../components/layout/page_hero';
 
 const query = `query{
-  projectPage(id:"U12v2mRNOc9EK6dewMY3L"){
-    projectHero{
-        projectHeroImage{
-          url
-          title
-        }
-        projectTitle
-        projectSubTitle
-        projectTags
-    }
-    projectSectionCollection(limit: 10){
-      items{
-          topicTitle
-          topicBody{
-            json
-          }
-          topicImage01{
-            url
-            title
-          }
-          topicImage02{
-            url
-            title
-          }
-        }
+  landingPage(id: "5hkVcf0Mo383BUmgvW22Gy") {
+    landingPageHero {
+      heroHeadline {
+        json
       }
-    projectCard{
-      projectThumbnail{
-        url
-        title
+      heroSubHeadline
+      introduction {
+        json
       }
-      projectTitle
-      projectSubtitle
-      projectTag
     }
   }
-  }
+}
 `;
 
 function Home() {
@@ -53,23 +27,15 @@ function Home() {
   if (errors) return <span style={{ color: "red" }}>{errors.map((error) => error.message).join(",")}</span>;
   if (!data) return <span>Loading...</span>;
 
-  const { projectPage } = data;
+  const { landingPage } = data;
 
-  console.log(projectPage)
-
-  const projectHero = projectPage.projectHero
-  const projectSection = projectPage.projectSectionCollection.items
+  const pageHero = landingPage.landingPageHero
 
   return (
-    <div className={styles.app} id='farid-portfolio'>
+    <div className={styles.c_container} id='farid-portfolio'>
       <Header />
-      <main className={styles.c_main} data-page-template="project">
-        <Hero type={projectHero} />
-        <section className={styles.c_body}>
-          {projectSection.map((section, index) => (
-            <Topic key={index} type={section} />
-          ))}
-        </section>
+      <main className={styles.c_main} data-page-template="landing page" data-page-theme="home" >
+        <Hero type={pageHero} />
       </main>
       <Footer />
     </div>
